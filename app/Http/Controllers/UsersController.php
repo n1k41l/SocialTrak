bf5fb85afa0586cee141bb891de2181e95a1a914
Nikhil Patel <nikhil.n.patel@gmail.com>
2015-04-16 17:59:18 +0000

    modify users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 
 use App\User;
@@ -23,6 +24,24 @@ class UsersController extends Controller {
 	public function show(User $user)
 	{
 		return view('users.show', compact('user'));
+	}
+
+	public function edit(User $user)
+	{
+		return view('users.edit', compact('user'));
+	}
+
+
+	/**
+	 * @param User $user
+	 * @param Request $request
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+	 */
+	public function update(User $user, UserRequest $request)
+	{
+		$user->update($request->all());
+
+		return redirect('/users/'.$user->id);
 	}
 
 	
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.application')
+
+@section('content')
+    <h1>Edit User: {!! $user->name !!}</h1>
+
+    @include('errors.errors')
+    {!! Form::model($user, ['method' => 'PATCH', 'action' => ['UsersController@update', $user->id]]) !!}
+    <!-- Name Form Input -->
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <!-- Email Form Input -->
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::text('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <!-- Submit Form Button -->
+    <div class="form-group">
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+
+
+    {!! Form::close() !!}
+    
+@stop
